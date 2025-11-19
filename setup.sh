33774d5fe4f64dfc74d95e8bc3e628dbafaf2dd1
tomclab <36747405+tomclab@users.noreply.github.com>
2025-11-19 14:42:01 -0800

    update

--- setup.sh
+++ setup.sh
@@ -139,7 +139,7 @@ fi
 echo "# Install required software."
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y libopenblas-dev libatlas3-base libcamera-dev python3-opencv portaudio19-dev
+sudo apt install -y libopenblas-dev libatlas3-base libcamera-dev python3-opencv portaudio19-dev python3-picamera2
 sudo apt install -y util-linux procps hostapd iproute2 iw haveged dnsmasq iptables espeak
 
 
